123d342da11244811fd5ae8d1a04ad99573f5bb9
Oussama Galai <oucema03@gmail.com>
2023-08-07 02:18:01 +0100

    JSX

---
+++ Week1/Day3/myfirstapp/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './style.css';
+import SecondeConponent from './components/SecondeConponent';
+
+
+function App() {
+  return (
+    <div>
+      <h1 className="firstApp">Hello Dojo !</h1>
+      <h2>Thing  i need to do :</h2>
+      <SecondeConponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ Week1/Day3/myfirstapp/src/components/SecondeConponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function SecondeConponent() {
+  return (
+    <ul>
+        <li>Learn React</li>
+        <li>Climb Mt. Everest</li>
+        <li>Run a marathon</li>
+        <li>Feed the dogs</li>
+      </ul>
+  )
+}
