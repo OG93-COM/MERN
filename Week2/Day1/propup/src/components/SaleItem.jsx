cab3b00ff0175da457e9b599573b19bc8f9eed4b
Oussama Galai <oucema03@gmail.com>
2023-08-08 02:09:51 +0100

    Props

--- Week2/Day1/propup/src/components/SaleItem.jsx
+++ Week2/Day1/propup/src/components/SaleItem.jsx
@@ -4,11 +4,12 @@ const SaleItem = (props) => {
   return (
     <div>
       <h1>{props.theName}</h1>
-      <p>Age : {props.age}</p>
+      <p>Age : {props.age}, Born on { 2023 - props.age }</p>
       <p>Hair Color :{props.hairColor}</p>
 
     </div>
   )
 }
+
 
 export default SaleItem
