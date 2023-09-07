dc9159d5b3296d22d7554bea5a5c60acaebe5862
Oussama Galai <oucema03@gmail.com>
2023-09-07 20:52:06 +0100

    Author

--- Week5/Day1/authors/client/src/views/PageNotFound.jsx
+++ Week5/Day1/authors/client/src/views/PageNotFound.jsx
@@ -3,7 +3,7 @@ import {Link} from "react-router-dom"
 
 const PageNotFound = () => {
   return (
-    <div>
+    <div className="content">
         <h3>Opps, Page Not Found</h3>
         <Link to={"/"}>Back to home page</Link>
     </div>
